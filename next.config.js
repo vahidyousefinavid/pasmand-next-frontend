8ba3aef5fc7acbebe1687534e708595b5a4f861d
vahid yousefi <vahidyousefinavid7@gmail.com>
2025-05-14 18:30:28 +0330

    fix style

--- next.config.js
+++ next.config.js
@@ -2,10 +2,11 @@
 
 const withPWA = require("@ducanh2912/next-pwa").default({
   dest: "public",
-    disable: false,
-    register: true,
-    skipWaiting: true,
-  
+  disable: false,
+  register: true,
+  skipWaiting: true,
+  legacyBrowsers: true,
+
 });
 
 async function rewrites() {
